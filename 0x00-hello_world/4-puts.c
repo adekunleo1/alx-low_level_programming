029a32beb405e1f052d150085b833a5d6546eb39
Adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-03-10 15:23:17 -0800

    My four puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ *main - entry point
  *
- * Return: 0 after printing the function
+ *Return: 0 after printing the function
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"):
-		return (0):
+puts("\"Programming is like building a multilingual puzzle"):
+return (0):
 }
