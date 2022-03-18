f4ab0b27324b68feb65c26bf3af2113868e20def
Adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-03-17 23:31:41 -0700

    No Puthcat

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,16 @@
  
  int main(void)
  {
- putchar(");
- _putchar('p);
- _putchar('u');
- _putchar('t');
- _putchar('c');
- _putchar('h');
- _putchar('a');
- _putchar('r');
- _putchar('\n');
-
- return (0);
+ 
+ { putchar(");
+  _putchar('p);
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+ }
+  return (0);
  }
