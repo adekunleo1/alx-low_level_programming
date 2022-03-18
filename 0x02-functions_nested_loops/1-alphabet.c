16a11171357a3ef918e78dd0f01bd290bef6ec51
Adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-03-18 07:02:36 -0700

    Neste loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
+}
