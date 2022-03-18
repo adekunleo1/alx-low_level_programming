c47a460cbbe1fcaebaf5c313dd04f17a9f8ab729
Adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-03-17 23:27:30 -0700

    No Putchar should return

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return voic
+ * /
+ 
+ int main(void)
+ {
+ putchar(");
+ _putchar('p);
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
+
+ return (0);
+ }
