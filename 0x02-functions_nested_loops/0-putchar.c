0ddd23a0810499c17f9cfc60f485528fd8aec30e
Adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-03-18 00:59:20 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,15 +9,6 @@
 
 int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
 
 return (0);
 }
