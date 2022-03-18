d8e54574989d607495fabeef69323477a1f9402a
Adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-03-18 06:29:54 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,23 @@
-int _putchar(char c)
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success) return
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
