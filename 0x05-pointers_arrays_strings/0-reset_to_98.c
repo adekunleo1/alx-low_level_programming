0dc26b888cbeccb2ffe967002645197072397618
Adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-03-22 08:53:19 -0700

    My first 0 task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value *n points to.
+ * reset_to_98 - function that takes a pointer to an int as parameter and updates the value *n points to 98
  * @n: the number to modify.
  */
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef main.h
+#define main.h
 
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
