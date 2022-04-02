e1debb4f03a192476ed93e5ec7ec66b657f7e24b
Adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-04-02 06:23:34 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+ 
+ ​#​include​ ​<​stdio.h​>
+ 
+  
+ ​/*​* 
+ ​ * main - calculate largest prime of 612852475143 
+ ​ * Return: 0 
+ ​ ​*/ 
+ ​int​ ​main​(​void​) 
+ ​{ 
+ ​        ​long​ ​int​ x = ​612852475143​; 
+ ​        ​long​ ​int​ y; 
+  
+ ​        ​for​ (y = ​2​; y < x; y++) 
+ ​        { 
+ ​                ​if​ (x % y == ​0​) 
+ ​                { 
+ ​                        x = x / y; 
+ ​                } 
+ ​        } 
+ ​        ​printf​(​"​%ld​\n​"​, y); 
+ ​        ​return​ (​0​); 
+ ​}
