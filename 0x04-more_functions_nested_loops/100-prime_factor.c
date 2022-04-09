2478aff7d75b66588ed34ef9df9e598a36735a90
Adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-04-09 02:57:51 -0700

    Advanced tasks

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - calculate largest prime of 612852475143
- * Retur: 0
+ * main - Entry point.
+ *
+ * Return: Always 0.
  */
+
 int main(void)
+
 {
-	long int x = 612852475143;
-	long int y;
-	
-	for (y = 2; y < x; y++)
-	{
-		if (x % y == 0)
-		{
-			x = x / y;
-		}
-	}
-	printf("%ld\n", y);
-	return (0);
+long n, i;
+
+n = 612852475143;
+for (i = 2; i < n; i++)
+{
+while (n % i == 0)
+n = n / i;
+}
+
+printf("%lu\n", n);
+
+return (0);
 }
