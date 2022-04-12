ded9d75abec543c4890ccdf002944094e8db71c0
Adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-04-12 03:15:38 -0700

    Static libraries

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if int c is a digit
+ * @c: integer c
+ * Return: 1 if true, otherwise 0
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c < 58);
+}
