971386fdff8f07098cc778787f668cc0663435c2
adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-06-18 06:31:07 -0700

    Main H correction

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,4 +1,5 @@
-#ifndef MAIN_H                                        #define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,14 +11,13 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *s);
+void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-int _strncmp(char *s1, char *s2, int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n); char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
